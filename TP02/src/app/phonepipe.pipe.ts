5e4431ff84393c00917dcb5182104b7c2e4db96a
Hugo Zigha <hugo.zigha@al-enterprise.com>
2020-09-26 15:32:59 +0200

    pipe fini et avancement recap

--- TP02/src/app/phonepipe.pipe.ts
+++ TP02/src/app/phonepipe.pipe.ts
@@ -8,21 +8,20 @@ export class PhonepipePipe implements PipeTransform {
   transform(value: any, country: any): any {
     if (value) {
       if (country.code === 'FR') {
-        let newStr = '';
+        let newStr = value.charAt(0) + ' ';
         let i = 0;
-        for (; i < Math.floor(value.length / 2) ; i++) {
+        for (; i < Math.floor(value.length / 2) + 1 ; i++) {
           newStr = newStr + value.substr(i * 2, 2) + ' ';
         }
-        return newStr ;    
-      }
-      else {
+        return newStr;
+      } else {
         let newStr = '';
         let i = 0;
         for (; i < Math.floor(value.length / 3) - 1; i++) {
           newStr = newStr + value.substr(i * 3, 3) + '-';
         }
         return newStr + value.substr(i * 3);
-      }    
+      }
     }
 
   }
